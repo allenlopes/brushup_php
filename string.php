91051ecb76e700cf28ca8d36718a2fdfb48b50b7
sudoallenlopes23 <allenlopes10@gmail.com>
2023-08-05 01:06:22 +0530

    string.php completed

--- string.php
+++ string.php
@@ -1,6 +1,20 @@
 <?php
 
-$name="Vishal";
-echo "Hello my name is $name";
+$name="Hi Hello, I am Allen";
+// echo "Hello my name is $name";
+
+// echo strlen($name); //string length
+
+// echo str_word_count($name); // word count in string
+
+// echo strpos($name, "I"); // As we have defined "I" it will give the position no. of "I" in the string $name.
+
+// echo ucfirst($name); //Make a string's first character uppercase
+
+// echo ucwords($name); //Uppercase the first character of each word in a string
+
+// echo strtoupper($name); //Make the whole string uppercase
+
+// echo strtolower($name); // Make the whole string lowercase
 
 ?>
