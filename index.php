e170dc78151247593a8f2731f7fa85aa4cfb9943
sudoallenlopes23 <allenlopes10@gmail.com>
2023-08-05 00:50:44 +0530

    Committing all files

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+// echo "vishal";
+
+// Variables (Stores the information)
+
+// ${_, a-z} {_, a-z, 0-9}
+
+// $x;
+// $x1;
+// $_x;
+// $1x - wrong way of defining variable
+
+$name = 'Vishal';
+$name = 'Amit';
+echo $name;
+
+?>
