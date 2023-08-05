b55fcdde80b1297c4ac634040bf950b192ea4340
sudoallenlopes23 <allenlopes10@gmail.com>
2023-08-05 18:57:27 +0530

    operators.php edited

---
+++ operators.php
@@ -0,0 +1,33 @@
+<?php
+
+$x = 10;
+$y = 20;
+
+//Arithmetic Operators ->
+// echo $x%$y;
+
+
+// Assignment Operators ->
+// $x += $y;
+// echo $x;
+
+
+//Comparison Operators -> (Increment Decrement)
+
+// echo $x++;
+// echo "<br>";
+// echo $x;
+
+// echo "<br>";
+// echo ++$x;
+
+
+// Logical Operators ->
+
+$fname = "Allen";
+$lname = "Lopes";
+echo $fname.' '.$lname;
+
+
+
+?>
