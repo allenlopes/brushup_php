827aab0ebf0f5eed0e0666d26043dc8f294116db
sudoallenlopes23 <allenlopes10@gmail.com>
2023-08-05 20:17:14 +0530

    constants.php edited

---
+++ constants.php
@@ -0,0 +1,14 @@
+<?php
+
+
+// code
+define('NUM', 10);
+echo NUM;
+
+
+// code
+define('NUM', 10);
+define('NUM', 20); // it will throw an error since 'NUM' is already defined earlier
+echo NUM;
+
+?>
