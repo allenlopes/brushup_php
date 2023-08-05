969c82e3b6d37a778e1ee7e83a8e2d6169a7da53
sudoallenlopes23 <allenlopes10@gmail.com>
2023-08-05 19:00:42 +0530

    string.php edited

--- string.php
+++ string.php
@@ -1,20 +1,25 @@
 <?php
 
 $name="Hi Hello, I am Allen";
-// echo "Hello my name is $name";
+echo "Hello my name is $name";
 
-// echo strlen($name); //string length
+echo strlen($name); //string length
 
-// echo str_word_count($name); // word count in string
+echo str_word_count($name); // word count in string
 
-// echo strpos($name, "I"); // As we have defined "I" it will give the position no. of "I" in the string $name.
+echo strpos($name, "I"); // As we have defined "I" it will give the position no. of "I" in the string $name.
 
-// echo ucfirst($name); //Make a string's first character uppercase
+echo ucfirst($name); //Make a string's first character uppercase
 
-// echo ucwords($name); //Uppercase the first character of each word in a string
+echo ucwords($name); //Uppercase the first character of each word in a string
 
-// echo strtoupper($name); //Make the whole string uppercase
+echo strtoupper($name); //Make the whole string uppercase
 
-// echo strtolower($name); // Make the whole string lowercase
+echo strtolower($name); // Make the whole string lowercase
+
+
+$fname= "Allen";
+$lname = "Lopes";
+echo $fname.' '.$lname
 
 ?>
