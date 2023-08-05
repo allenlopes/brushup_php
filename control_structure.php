c90e936f7b77d6bf6f972202ff5c3e6856ff53cb
sudoallenlopes23 <allenlopes10@gmail.com>
2023-08-05 19:39:21 +0530

    control_structure.php edited

---
+++ control_structure.php
@@ -0,0 +1,43 @@
+<?php
+/*
+$x = 10;
+if($x != 20)
+{
+    echo "Yes";
+}
+else
+{
+    echo "No";
+}
+*/
+
+/*
+$number = 70;
+
+if($number > 70)
+{
+    echo "First";
+}
+else
+{
+    echo "Other";
+}
+*/
+
+
+$num = 45;
+
+if($num > 60)
+{
+    echo "First";
+}
+else if($num >= 45 && $num <= 60)
+{
+    echo "Second";
+}
+else
+{
+    echo "Third";
+}
+
+?>
