567129416d149cfdca2605db1dc72fcf08b70acc
sudoallenlopes23 <allenlopes10@gmail.com>
2023-08-06 21:17:50 +0530

    arrayFunctions.php completed

---
+++ arrayFunctions.php
@@ -0,0 +1,24 @@
+<?php
+
+//
+$arr = array("Vishal","Amit","Arun");
+echo count($arr); // Gives the count of values inside the array.
+
+echo '<br>';
+//
+$arr = array("Vishal","Amit","Arun");
+sort($arr); // Sorts the array according to alphabetical order from the values inside the array.
+print_r($arr);
+
+echo '<br>';
+//
+$arr = array("Vishal","Amit","Arun");
+ksort($arr); // Sorts the array in reverse order from the values inside the array.
+print_r($arr);
+
+echo '<br>';
+
+
+?>
+
+
