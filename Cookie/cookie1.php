a295f3109af4297d97b0735b25b711f71201d9ca
sudoallenlopes23 <allenlopes10@gmail.com>
2023-08-08 21:59:26 +0530

    Cookies Completed

---
+++ Cookie/cookie1.php
@@ -0,0 +1,6 @@
+<?php
+
+// setcookie(cookie Name, cookie Value, Cookie time till which it should be in user's browser)
+setcookie('city','Delhi',time()+150); // cookie running for 10 secs.
+
+?>
---
+++ Cookie/cookie3_delete.php
@@ -0,0 +1,7 @@
+<?php
+
+// For deleting cookie ->
+// setcookie(cookie Name to be deleted, cookie Value to be deleted, Cookie time or just default value 100 secs.)
+setcookie('city', 'Delhi', 100);
+
+?>
