902d7bd1a9e9fb31cb3b5d5cfb8f23109ee13873
sudoallenlopes23 <allenlopes10@gmail.com>
2023-08-09 22:50:44 +0530

    arrayFunctions.php modified little

--- arrayFunctions.php
+++ arrayFunctions.php
@@ -4,11 +4,13 @@
 $arr = array("Vishal","Amit","Arun");
 echo count($arr); // Gives the count of values inside the array.
 
+
 echo '<br>';
 //
 $arr = array("Vishal","Amit","Arun");
 sort($arr); // Sorts the array according to alphabetical order from the values inside the array.
 print_r($arr);
+
 
 echo '<br>';
 //
